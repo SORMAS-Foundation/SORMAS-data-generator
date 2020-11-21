0d752a13ae364c1d29c30b868354405935a3c099
Jonas Cirotzki <s8jociro@stud.uni-saarland.de>
2020-11-21 13:22:45 +0100

    download working 1.49.1 release

--- data-gen/gen-client.sh
+++ data-gen/gen-client.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-#tmp=$(mktemp -d)
-#echo $tmp
-#wget https://github.com/hzi-braunschweig/SORMAS-Project/releases/download/v1.49.1/sormas_1.49.1.zip -P "$tmp"
-#unzip "$tmp/sormas_1.49.1.zip" -d "$tmp"
-#cp "$tmp/deploy/openapi/sormas-rest.yaml" .
-#rm -rf "$tmp"
+source ../.env
+tmp=$(mktemp -d)
+echo $tmp
+wget "https://github.com/hzi-braunschweig/SORMAS-Project/releases/download/v${SORMAS_VERSION}/sormas_${SORMAS_VERSION}.zip" -P "$tmp"
+unzip "$tmp/sormas_${SORMAS_VERSION}.zip" -d "$tmp"
+cp "$tmp/deploy/openapi/sormas-rest.yaml" .
+rm -rf "$tmp"
 
 
-#sed -i 's/http-basic/basicAuth/g' sormas-rest.yaml
-#sed -i 's/[[:space:]]Basic/\ basic/g' sormas-rest.yaml
+sed -i 's/http-basic/basicAuth/g' sormas-rest.yaml
+sed -i 's/[[:space:]]Basic/\ basic/g' sormas-rest.yaml
 
 
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
@@ -22,7 +23,7 @@ sudo chown -R $USER:$USER out
 python3 -m venv venv
 source ./venv/bin/activate
 
- pip install -r requirements.txt
+pip install -r requirements.txt
 
 pushd out || exit
 python3 setup.py install --record files.txt
