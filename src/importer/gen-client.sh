a6086a66e35ae539f2063a4e23e019f604d3c29a
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2021-02-22 15:30:56 +0100

    remove openapi auth work around

--- src/importer/gen-client.sh
+++ src/importer/gen-client.sh
@@ -11,18 +11,15 @@ unzip "$tmp/sormas_${SORMAS_VERSION}.zip" -d "$tmp"
 cp "$tmp/deploy/openapi/sormas-rest.yaml" .
 rm -rf "$tmp"
 
-
 python3 -m venv venv
 source ./venv/bin/activate
 # FIXME Debian repo version is to old
-pip3 install --upgrade wheel setuptools pyyaml
+pip3 install --upgrade wheel setuptools
 
 # use docker if we run on the host for development
 if [[ -z "${DOCKERIZED}" ]]; then
-  # https://github.com/hzi-braunschweig/SORMAS-Project/issues/3293
-  python3 fix_yaml.py
   docker run --rm -v "${PWD}:/local" "openapitools/openapi-generator-cli:v${OPENAPI_GENERATOR_VERSION}" generate \
-    -i local/sormas-rest-fixed.yaml \
+    -i local/sormas-rest.yaml \
     -g python \
     -o /local/out \
     --package-name sormas
@@ -31,17 +28,15 @@ if [[ -z "${DOCKERIZED}" ]]; then
   PIP_REQ_PATH=requirements.txt
 
 else
-  # https://github.com/hzi-braunschweig/SORMAS-Project/issues/3293
-  python3 importer/fix_yaml.py
 
   # https://github.com/OpenAPITools/openapi-generator#launcher-script
   mkdir -p ~/bin/openapitools
-  curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > ~/bin/openapitools/openapi-generator-cli
+  curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh >~/bin/openapitools/openapi-generator-cli
   chmod u+x ~/bin/openapitools/openapi-generator-cli
   export PATH=$PATH:~/bin/openapitools/
 
   openapi-generator-cli generate \
-    -i sormas-rest-fixed.yaml \
+    -i sormas-rest.yaml \
     -g python \
     -o out \
     --package-name sormas
@@ -50,11 +45,8 @@ else
 
 fi
 
-
-
 pip3 install -r $PIP_REQ_PATH
 
 pushd out || exit
 pip3 install .
 popd || exit
-
